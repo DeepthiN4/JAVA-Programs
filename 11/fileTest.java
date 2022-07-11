8ffee0cbde01ababfd43b8e617687b991271bc02
S Deepthi Nayak <88554722+DeepthiN4@users.noreply.github.com>
2022-07-11 22:34:09 +0530

    Add files via upload

---
+++ 11/fileTest.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+class fileTest{
+    public static void main(String args[]) throws IOException{
+        int i;
+        FileInputStream fin = null;
+        FileOutputStream fout = null;
+
+        if(args.length!=2){
+            System.out.println("Usage:copyfile from to");
+            return;
+        }
+
+        try {
+            fin = new FileInputStream(args[0]);
+            fout = new FileOutputStream(args[1]);
+
+            do {
+                i = fin.read();
+                if (i != -1) fout.write(i);
+            } while (i != -1);
+        }
+        catch(IOException e)
+        {
+            System.out.println("I/o error."+e);
+        }
+        finally{
+            try{
+                if(fin!=null) fin.close();
+            }
+            catch(IOException e2){
+                System.out.println("Error closing input file.");
+            }
+            try{
+                if(fout!=null) fout.close();
+            }
+            catch(IOException e2){
+                System.out.println("Error closing output file.");
+            }
+
+        }
+    }
+}
